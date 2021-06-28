327c3c2c15805589e5ab0b2abaff911fd0f99ce2
ashishvats028 <ashishvats028@gmail.com>
2021-06-28 13:19:13 +0530

    Updated Login/Profile/Home pages

---
+++ src/Config.js
@@ -0,0 +1,11 @@
+export const properties = {
+    dpUrl: "https://instagram.fdel27-3.fna.fbcdn.net/v/t51.2885-19/s150x150/14533632_1786993144896695_7436885136022437888_a.jpg?tp=1&_nc_ht=instagram.fdel27-3.fna.fbcdn.net&_nc_ohc=8xQKhk0WuBQAX8j7aOE&edm=ABfd0MgBAAAA&ccb=7-4&oh=44a404dd30d991a2720167c1a8fac2c9&oe=60DF26DF&_nc_sid=7bff83",
+    username: "ashidummies",
+    accessToken: "IGQVJYU0RaSlJzdW5ndWNZAeWs3VEZAOV09TTTdCYmZANenNjSTdSSnNUazYwTHRmZAEdFcVlWQmExT1NZATXBtXy1jZAkg3OUI5eUVBLW5TTTB6cXY4Yy0zVDdZAOUc2Y0JZAZAzJIazVEOHFjeDJEaFM4TFdGSwZDZD",
+    fullName: "Ashish Vats",
+    hashTags: "#upgrad #beautiful #amazing"
+    /*
+    username: "upgrad",
+    password: "upgrad"
+    */ // Hard-Coded in Login.js as per instructions.
+};
